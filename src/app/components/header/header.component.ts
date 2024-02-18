a177e712e8c3c1cd29f684853a87e6fe5b6a7224
luizhakan <luizhakan2@protonmail.com>
2024-02-18 14:08:37 -0300

    Ajuste na responsividade e remoção da sidebar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isSmallScreen = false;
 
+  ngOnInit() {
+    if (window.innerWidth < 768) {
+      this.isSmallScreen = true;
+      console.log('small screen');
+    } else {
+      this.isSmallScreen = false;
+      console.log('big screen');
+    }
+  }
 }
